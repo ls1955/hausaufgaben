2c831d60146823f153a8995cdb31c6386dccbebc
Cheong <xxlsxx1955@gmail.com>
2023-12-18 17:59:00 +0800

    Ask READ_EXTERNAL_STORAGE permission
    Ask the permission in order to access the files.

--- App.jsx
+++ App.jsx
@@ -1,5 +1,30 @@
-import { Text } from "react-native";
+import {useEffect} from 'react';
+import {PermissionsAndroid, Text} from 'react-native';
 
 export default function App() {
-  return <Text>Goodbye, world.</Text>
+  useEffect(() => {
+    askPermission();
+  }, []);
+
+  return <Text>Goodbye, world.</Text>;
 }
+
+// Asks for storage access permission, return true if granted permission, else false.
+const askPermission = async () => {
+  const hasPermission = PermissionsAndroid.check(
+    PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+  );
+
+  if (hasPermission) return true;
+
+  const granted = PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+    {
+      title: 'hausaufgaben read storage permission',
+      message: 'To read your homework folders',
+      buttonPositive: 'Ok',
+    },
+  );
+
+  return granted === PermissionsAndroid.RESULTS.GRANTED;
+};
