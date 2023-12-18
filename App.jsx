219cd5d8063921834ae5943a6d538a082332d98b
Cheong <xxlsxx1955@gmail.com>
2023-12-19 06:57:35 +0800

    Fix askPermission bug
    Forgot to slap on await when checking and requesting permission.

--- App.jsx
+++ App.jsx
@@ -11,13 +11,13 @@ export default function App() {
 
 // Asks for storage access permission, return true if granted permission, else false.
 const askPermission = async () => {
-  const hasPermission = PermissionsAndroid.check(
+  const hasPermission = await PermissionsAndroid.check(
     PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
   );
 
   if (hasPermission) return true;
 
-  const granted = PermissionsAndroid.request(
+  const granted = await PermissionsAndroid.request(
     PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
     {
       title: 'hausaufgaben read storage permission',
