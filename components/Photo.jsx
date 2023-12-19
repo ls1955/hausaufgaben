df05ebddfc501d1310d7d5a9354eb2095386010a
Cheong <xxlsxx1955@gmail.com>
2023-12-19 14:56:37 +0800

    Include Photo component
    A component that take a list of uris and show one photo at a time.
    Definitely have to add some explanation for that component...

---
+++ components/Photo.jsx
@@ -0,0 +1,29 @@
+import {Image, View} from 'react-native';
+import Swiper from 'react-native-swiper';
+
+// A component that support swiping across photos while showing one at the time.
+export default function Photo({uris, status}) {
+  const photos = uris.map((uri, i) => {
+    return (
+      <View
+        key={i}
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Image
+          style={{width: '100%', flex: 1}}
+          resizeMode="contain"
+          source={{uri}}
+        />
+      </View>
+    );
+  });
+
+  return (
+    <Swiper loop={false} index={status['selectedPhotoIndex']}>
+      {photos}
+    </Swiper>
+  );
+}
