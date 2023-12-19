7e84d1e8e882ddeec257195f2ebb49375bb46be9
Cheong <xxlsxx1955@gmail.com>
2023-12-19 09:56:03 +0800

    Include App state
    As we get into the fun part, where the app will be in various status
    according to what component have be clicked.

--- components/Folder.jsx
+++ components/Folder.jsx
@@ -1,9 +1,16 @@
 import {Text, TouchableOpacity, View} from 'react-native';
 
-export default function Folder({name, uris}) {
+export default function Folder({name, uris, status, onStatus}) {
+  // Update the state before going into folder's photos
+  handleStatus = () => {
+    console.log("SHould go into the photos...")
+    onStatus({...status, state: 'photos', selectedFolder: name});
+  };
+
   return (
     <View style={{alignItems: 'center', marginBottom: 20}}>
       <TouchableOpacity
+        onPress={handleStatus}
         style={{
           width: 100,
           height: 100,
