668fe90a52a3cf74fe3ef586a000eb940454ef4f
Cheong <xxlsxx1955@gmail.com>
2023-12-19 18:37:06 +0800

    Include some nonsensical comments
    Guess author will call it a day here...

--- components/Album.jsx
+++ components/Album.jsx
@@ -1,13 +1,23 @@
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 
 // A component that contains folders.
-export default function Album({name}) {
-  // TODO: Include folders thumbnails?
+export default function Album({name, status, onStatus}) {
+  // TODO: Include thumbnails consists of folders
+  // TODO: Include an onpressed event that update the status
 
   return (
-    <View style={{flex: 1,alignItems: 'center', marginBottom: 20}}>
-      <View style={{width: '90%', height: 110, backgroundColor: "yellow"}}></View>
+    <View style={{flex: 1, alignItems: 'center', marginBottom: 20}}>
+      <TouchableOpacity
+        style={{width: '90%', height: 110, backgroundColor: 'grey'}}
+      />
       <Text>{name}</Text>
     </View>
   );
 }
+
+// TOWRITE:
+// An album that contain the name and thumbnails of its folders
+// Should update the status when itself is being clicked, such that folder
+// underneath is display
+// Should also include flag to notify that its going into folder from album
+// Looks like we need another state here.
