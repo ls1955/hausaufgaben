306cbaf78e73ae306b00f68f45012aa6721e84fd
Cheong <xxlsxx1955@gmail.com>
2023-12-20 08:27:18 +0800

    Include fromAlbum prop
    The last pieces required to run the finite-state machine... There is
    probably better way of doing stuff, since author didn't know a lick
    about React...
    
    Lets just settle for this for now...

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,13 +1,15 @@
 import ImageView from 'react-native-image-viewing';
 
 // A component that takes a lits of uris, and show one photo at the time. Support swiping and zooming.
-export default function Photo({uris, status, onStatus}) {
+export default function Photo({uris, status, onStatus, fromAlbum = false}) {
+  const nextState = fromAlbum ? 'inFolderFromAlbum' : 'inFolder';
+
   return (
     <ImageView
       images={uris.map(uri => ({uri}))}
       imageIndex={status['selectedPhotoIndex']}
       visible={true}
-      onRequestClose={() => onStatus({...status, state: 'inFolder'})}
+      onRequestClose={() => onStatus({...status, state: nextState})}
       HeaderComponent={() => null}
     />
   );
