0f478dfa97111647ac3889a556de13ea56f089a2
Cheong <xxlsxx1955@gmail.com>
2023-12-20 12:35:02 +0800

    Extract nonGroupFolders
    Letting it sitting inside the App.jsx doesn't sounds right.

---
+++ app_configs.js
@@ -0,0 +1,6 @@
+// This file contains various configurations related to the App itself.
+
+// Folders that do not wanna be put into a group.
+const nonGroupFolders = new Set(['Download', 'Whatsapp', '相机', '下载']);
+
+export {nonGroupFolders};
