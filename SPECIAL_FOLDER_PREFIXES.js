9813e041f4eaabb6ce76e7682c0e95b50d27c499
Cheong <xxlsxx1955@gmail.com>
2023-12-21 17:16:48 +0800

    Include SPECIAL_FOLDER_PREFIXES.js
    Preparation for auto grouping feature.

---
+++ SPECIAL_FOLDER_PREFIXES.js
@@ -0,0 +1,4 @@
+// Folders that start with any of these prefix will be grouped into their own group.
+// Although it should be put inside the appConfig.js, for privacy reason author decided to isolate
+// it out. By default it is empty.
+export default SPECIAL_FOLDER_PREFIXES = [];
