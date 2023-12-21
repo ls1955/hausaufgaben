91638ec0337cc19532576dbbcd7c030ac243c75e
Cheong <xxlsxx1955@gmail.com>
2023-12-21 14:04:36 +0800

    Implement lazy loading
    Only load the image URIs when inside the pages that need to show the
    images.
    
    What a great experience on useState, useEffect and async.

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import {BackHandler} from 'react-native';
+import {BackHandler, Text} from 'react-native';
 
 import HomePage from './components/HomePage';
 import AlbumFoldersPage from './components/AlbumFoldersPage';
@@ -9,6 +9,7 @@ import PhotoPage from './components/PhotoPage';
 import getPermission from './initializers/getPermission';
 import loadFolders from './initializers/loadFolders';
 import getGroupedAlbums from './initializers/getGroupedAlbums';
+import {CameraRoll} from '@react-native-camera-roll/camera-roll';
 
 export default function App() {
   const [folders, setFolders] = useState({});
@@ -61,16 +62,39 @@ export default function App() {
     return () => backHandler.remove();
   }, [status]);
 
-  // start rendering the page according to state...
-  let uris = folders[status['selectedFolder']]?.['imageUris'];
-  let isFromAlbum = false;
+  useEffect(() => {
+    // caches the image URIs if it hasn't been cache before, else do nothing.
+    const cacheImageUris = async () => {
+      if (status['selectedFolder'] == null) return;
+      if (folders[status['selectedFolder']]['imageUris'] != null) return;
 
+      const newFolders = {...folders};
+
+      newFolders[status['selectedFolder']]['imageUris'] = await getImageUris({
+        folderTitle: status['selectedFolder'],
+      });
+      setFolders(newFolders);
+    };
+    cacheImageUris();
+  }, [status]);
+
+  // load the pages that doesn't require preloading image Uris
   switch (status['state']) {
     case 'home':
       return HomePage({albums, folders, status, onStatus: setStatus});
     case 'inAlbum':
-      const folderTitles = [...albums[status['selectedAlbum']]]
+      const folderTitles = [...albums[status['selectedAlbum']]];
       return AlbumFoldersPage({folderTitles, status, onStatus: setStatus});
+  }
+
+  const uris = folders[status['selectedFolder']].imageUris;
+
+  // React will load the image Uris if it hasn't load before.
+  if (uris == null) return <Text>Loading a page here...</Text>;
+
+  let isFromAlbum = false;
+
+  switch (status['state']) {
     case 'inFolderFromAlbum':
       isFromAlbum = true;
     case 'inFolder':
@@ -81,3 +105,17 @@ export default function App() {
       return PhotoPage({uris, status, onStatus: setStatus, isFromAlbum});
   }
 }
+
+// TODO: Move this somewhere else
+const MAX_IMAGE_PER_FOLDER = 300;
+
+// TODO: Move this somewhere else.
+// Returns an array of image URIs (String) of given folderTitle.
+const getImageUris = async ({folderTitle}) => {
+  const photos = await CameraRoll.getPhotos({
+    first: MAX_IMAGE_PER_FOLDER,
+    groupName: folderTitle,
+  });
+
+  return photos.edges.map(edge => edge.node.image.uri);
+};
