a91b488c73f31204f8fd4be13e97cce26cabbf65
Cheong <xxlsxx1955@gmail.com>
2023-12-21 09:05:04 +0800

    Unify filename and variable convention
    For some reason filename was in snake_case, make it camelCase.
    
    nonGroupFolders -> NON_GROUP_FOLDERS
    
    To signify it is a constant.

--- appConfigs.js
+++ appConfigs.js
@@ -1,6 +1,6 @@
 // This file contains various configurations related to the App itself.
 
 // Folders that do not wanna be put into a group.
-const nonGroupFolders = new Set(['Download', 'Whatsapp', '相机', '下载']);
+const NON_GROUP_FOLDERS = new Set(['Download', 'Whatsapp', '相机', '下载']);
 
-export {nonGroupFolders};
+export {NON_GROUP_FOLDERS};
