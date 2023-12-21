315d9600a6854dbe0451e5bfb5f3e82fb3736999
Cheong <xxlsxx1955@gmail.com>
2023-12-21 17:28:48 +0800

    Put it back appConfig.js

--- SPECIAL_FOLDER_PREFIXES.js
+++
@@ -1,4 +0,0 @@
-// Folders that start with any of these prefix will be grouped into their own group.
-// Although it should be put inside the appConfig.js, for privacy reason author decided to isolate
-// it out. By default it is empty.
-export default SPECIAL_FOLDER_PREFIXES = [];
