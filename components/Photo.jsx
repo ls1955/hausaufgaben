e20cde075a85db8f6cb40385ee21b6ad659790e2
Cheong <xxlsxx1955@gmail.com>
2023-12-24 09:42:18 +0800

    Use react-native-image-gallery
    Although there is a bunch of errors and warnings inside the terminal,
    it works. For now lets just use that package for now. Since
    react-native-image-viewing has some bugs that hasn't been fixed for a
    while now...

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,16 +1,17 @@
-import ImageView from 'react-native-image-viewing';
+import Gallery from 'react-native-image-gallery';
 
 // A component that takes a lits of uris, and show one photo at the time. Support swiping and zooming.
 export default function Photo({uris, status, onStatus, isFromAlbum}) {
   const nextState = isFromAlbum ? 'inFolderFromAlbum' : 'inFolder';
 
+  const images = uris.map(uri => ({source: {uri}}));
+
   return (
-    <ImageView
-      images={uris.map(uri => ({uri}))}
-      imageIndex={status['selectedPhotoIndex']}
-      visible={true}
+    <Gallery
+      style={{flex: 1, backgroundColor: 'black'}}
+      images={images}
+      initialPage={status['selectedPhotoIndex']}
       onRequestClose={() => onStatus({...status, state: nextState})}
-      HeaderComponent={() => null}
     />
   );
 }
