5adf4842eaae3a6d69ad999ab7e7e816cd1aeca7
Cheong <xxlsxx1955@gmail.com>
2023-12-24 16:24:56 +0800

    Revert back to react-native-image-viewing
    Use the package that include fixes that author need.

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,17 +1,16 @@
-import Gallery from 'react-native-image-gallery';
+import ImageView from 'react-native-image-viewing';
 
 // A component that takes a lits of uris, and show one photo at the time. Support swiping and zooming.
 export default function Photo({uris, status, onStatus, isFromAlbum}) {
   const nextState = isFromAlbum ? 'inFolderFromAlbum' : 'inFolder';
 
-  const images = uris.map(uri => ({source: {uri}}));
-
   return (
-    <Gallery
-      style={{flex: 1, backgroundColor: 'black'}}
-      images={images}
-      initialPage={status['selectedPhotoIndex']}
+    <ImageView
+      images={uris.map(uri => ({uri}))}
+      imageIndex={status['selectedPhotoIndex']}
+      visible={true}
       onRequestClose={() => onStatus({...status, state: nextState})}
+      HeaderComponent={() => null}
     />
   );
 }
