a324503910a454741af0e606765d382e7f175114
Cheong <xxlsxx1955@gmail.com>
2023-12-28 15:09:44 +0800

    Include ActivityIndicator
    We like to see something spinning while loading something, right?

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,10 @@
-import {View, Text} from 'react-native';
+import {ActivityIndicator, View, Text} from 'react-native';
+import { DarkTheme } from '@react-navigation/native';
 
 export default function Loading() {
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <ActivityIndicator color={DarkTheme.colors.text} size={50} />
       <Text>Loading, please wait...</Text>
     </View>
   );
