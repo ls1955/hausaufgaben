fd80c88609c4132851b9e6d7d877db6faf9051c8
Cheong <xxlsxx1955@gmail.com>
2023-12-28 14:48:43 +0800

    Change Photo to MediaScreen

---
+++ src/screens/MediaScreen.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import ImageView from 'react-native-image-viewing';
+
+import { FoldersContext } from '../../contexts/FoldersContext';
+
+// The screen that shows media one at a time.
+export default function MediaScreen({navigation, route: {params}}) {
+  const {index, folderTitle} = params;
+  const mediaUris = useContext(FoldersContext)[folderTitle].imageUris;
+
+  const handleGoBack = () => navigation.goBack();
+
+  return (
+    <ImageView
+      images={mediaUris.map(uri => ({uri}))}
+      imageIndex={index}
+      visible={true}
+      onRequestClose={handleGoBack}
+      HeaderComponent={() => null}
+    />
+  );
+}
