19d7ab3cd5d3871c0321d023960fc1ee88f3f3c2
Cheong <xxlsxx1955@gmail.com>
2023-12-30 08:10:14 +0800

    Include Album and Folder count
    Show the count under each album and folder cover.
    
    This commit also:
    * Include reason why count attribute is kept inside FoldersContext

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 
+import { AlbumsContext } from '../../contexts/AlbumsContext';
 import {formatTitle} from '../../utils';
 
 // A component that represents an album cover.
 export default function Album({title, navigation}) {
   // TODO: Include thumbnails consists of folders
+
+  const count = useContext(AlbumsContext)[title].size;
 
   const handleNav = () => navigation.navigate('AlbumFolders', {title});
 
@@ -14,7 +18,8 @@ export default function Album({title, navigation}) {
         onPress={handleNav}
         style={{width: 110, height: 110, backgroundColor: 'white'}}
       />
-      <Text>{formatTitle({title})}</Text>
+      <Text style={{fontWeight: "bold"}}>{formatTitle({title})}</Text>
+      <Text style={{fontSize: 12, marginTop: 2}}>{count}</Text>
     </View>
   );
 }
--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,10 +1,14 @@
+import {useContext} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 
+import {FoldersContext} from '../../contexts/FoldersContext';
 import {formatTitle} from '../../utils';
 
 // A component that represent a folder cover.
 export default function Folder({title, navigation}) {
   const handleNav = () => navigation.navigate('FolderContents', {title});
+
+  const count = useContext(FoldersContext)[title].count;
 
   return (
     <View style={{alignItems: 'center', marginBottom: 20, marginRight: 15}}>
@@ -12,7 +16,8 @@ export default function Folder({title, navigation}) {
         onPress={handleNav}
         style={{width: 110, height: 110, backgroundColor: 'white'}}
       />
-      <Text>{formatTitle({title})}</Text>
+      <Text style={{fontWeight: "bold"}}>{formatTitle({title})}</Text>
+      <Text style={{fontSize: 12, marginTop: 2}}>{count}</Text>
     </View>
   );
 }
