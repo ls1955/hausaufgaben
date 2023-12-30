acc5434b7f6fb789625df88921e0c61fca6e1189
Cheong <xxlsxx1955@gmail.com>
2023-12-30 14:47:35 +0800

    Include PERMISSION_REQUIRED_DIRECTORIES
    Include another constant, where its purpose is explain with comment
    above it.

--- appConfigs.js
+++ appConfigs.js
@@ -19,6 +19,20 @@ const NON_GROUP_FOLDERS = new Set(['Download', 'Whatsapp', '相机', '下载']);
 // Folders that start with any of these prefix will be grouped into their own group.
 const SPECIAL_FOLDER_PREFIXES = [];
 
+// Directories that will require scoped storage permissions in order to make use of Organize Download
+// folder feature. User could give permission to custom path for each directory, as long as those
+// path align with the purpose, it will work as expected.
+//
+// downloadDir: Usually the download folder on phone. It will be the source of files to organize from.
+// defaultDir: The directory to use for storing general folder. For author use case, it is somewhere
+// inside his SD card.
+// doujinDir: The directory to store doujin. Could also be the same directory with defaultDir
+// stagingDir: The directory to store folder that will be move to somewhere else. For author use case,
+// this is where he puts the folder that take too much space on his tiny phone, before moving them to
+// HDD later on at once.
+// 
+const PERMISSION_REQUIRED_DIRECTORIES = ['downloadDir', 'defaultDir', 'doujinDir', 'stagingDir'];
+
 export {
   TOP_NAV_BAR_HEIGHT,
   GALLERY_FLAT_LIST_NUM_COLUMNS,
@@ -26,4 +40,5 @@ export {
   MAX_IMAGE_PER_FOLDER,
   NON_GROUP_FOLDERS,
   SPECIAL_FOLDER_PREFIXES,
+  PERMISSION_REQUIRED_DIRECTORIES
 };
