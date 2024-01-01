75136c821b8334f868a85d31b406d21c04c482c5
Cheong <xxlsxx1955@gmail.com>
2024-01-01 08:43:10 +0800

    Throw in some styles
    ...uh, author still looks pretty lame, guess author will apply some more
    style later on...

--- src/components/OrganizeDownloadModal.jsx
+++ src/components/OrganizeDownloadModal.jsx
@@ -1,12 +1,12 @@
-import {Pressable, Text, TextInput, View} from 'react-native';
+import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useContext, useState} from 'react';
-import { DarkTheme } from '@react-navigation/native';
+import {DarkTheme} from '@react-navigation/native';
 import CheckBox from '@react-native-community/checkbox';
 import SelectDropdown from 'react-native-select-dropdown';
 
 import {organizeDownloadFolder} from '../../utils';
 import {CATEGORY_OPTIONS} from '../../appConfigs';
-import { AlbumsContext } from '../../contexts/AlbumsContext';
+import {AlbumsContext} from '../../contexts/AlbumsContext';
 
 // A modal that shows up for user to organize his/her download folder.
 export default function OrganizeDownloadModal({navigation}) {
@@ -22,7 +22,12 @@ export default function OrganizeDownloadModal({navigation}) {
         console.log('Please enter a folderTitle or select a category');
         return;
       }
-      await organizeDownloadFolder({title: folderTitle, category, isToStaging, albums});
+      await organizeDownloadFolder({
+        title: folderTitle,
+        category,
+        isToStaging,
+        albums,
+      });
       // TODO: notify with a flash message.
     } catch (error) {
       // TODO: Warn with flash message.
@@ -32,13 +37,13 @@ export default function OrganizeDownloadModal({navigation}) {
   const handleClose = () => navigation.goBack();
 
   return (
-    <View style={{backgroundColor: DarkTheme.colors.background, top: '20%', padding: 20}}>
+    <View style={styles.modalContainer}>
       <View>
         <FolderTitleInput value={folderTitle} onChangeText={setFolderTitle} />
-        <Text>Or</Text>
+      <StagingCheckbox value={isToStaging} onValueChange={setIsToStaging} />
+        <Text style={styles.inBetweenText}>Or</Text>
         <CategorySelect onSelect={setCategory} />
       </View>
-      <StagingCheckbox value={isToStaging} onValueChange={setIsToStaging} />
       <ActionButtons onOrganize={handleOrganize} onCancel={handleClose} />
     </View>
   );
@@ -49,7 +54,8 @@ function FolderTitleInput({value, onChangeText}) {
     <TextInput
       value={value}
       onChangeText={onChangeText}
-      placeholder="folder-name"
+      placeholder="new-folder-title"
+      style={styles.folderTitleInput}
     />
   );
 }
@@ -60,14 +66,19 @@ function CategorySelect({onSelect}) {
       data={CATEGORY_OPTIONS}
       onSelect={onSelect}
       buttonTextAfterSelection={item => item}
-      defaultValue=""
+      defaultButtonText="select-a-category"
+      rowStyle={styles.selectRow}
+      buttonStyle={styles.selectButton}
+      rowTextStyle={styles.selectSelectedRow}
+      buttonTextStyle={styles.selectText}
+      selectedRowTextStyle={[styles.selectText, styles.selectSelectedRow]}
     />
   );
 }
 
 function StagingCheckbox({value, onValueChange}) {
   return (
-    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+    <View style={styles.checkBoxContainer}>
       <CheckBox value={value} onValueChange={onValueChange} />
       <Text>To Staging Folder</Text>
     </View>
@@ -76,7 +87,7 @@ function StagingCheckbox({value, onValueChange}) {
 
 function ActionButtons({onOrganize, onCancel}) {
   return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+    <View style={styles.buttonsContainer}>
       <Pressable onPress={onOrganize}>
         <Text>Organize</Text>
       </Pressable>
@@ -86,3 +97,42 @@ function ActionButtons({onOrganize, onCancel}) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    backgroundColor: 'rgb(40, 40, 40)',
+    marginTop: 'auto',
+    borderRadius: 10,
+    padding: 20,
+  },
+  inBetweenText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 0,
+  },
+  folderTitleInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+    paddingBottom: 0,
+    marginTop: 15,
+    marginBottom: 5,
+    fontSize: 20,
+  },
+  checkBoxContainer: {flexDirection: 'row', alignItems: 'center'},
+  selectRow: {
+    backgroundColor: 'rgb(40, 40, 40)',
+    color: DarkTheme.colors.text,
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+  },
+  selectSelectedRow: {
+    borderBottomWidth: 1, borderBottomColor: "white"
+  },
+  selectButton: {
+    backgroundColor: 'rgb(40, 40, 40)',
+    color: DarkTheme.colors.text,
+  },
+  selectText: {color: DarkTheme.colors.text},
+  buttonsContainer: {flexDirection: 'row', justifyContent: 'space-around', marginTop: 15}
+});
